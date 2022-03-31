6b35f081fb4bf3460e983cc7bd092bc2b49d9208
kscbxxLiuXP <kscbxxLiuXP@126.com>
2022-03-31 09:09:57 +0800

    更新 在线调试窗口更新V1.0

---
+++ src/components/Popover/MyPopover.vue
@@ -0,0 +1,87 @@
+<template>
+    <span>
+        <transition
+            name="fade-in-linear">
+            <div v-show="showPopper" ref="popover" @mouseover="onMouseOver" @mouseout="onMouseOut" class="popper">
+                {{ title }}
+            </div>
+        </transition>
+        <span ref="trigger">
+            <slot></slot>
+        </span>
+    </span>
+</template>
+
+<script>
+// import 'vue-popperjs/dist/vue-popper.css';
+
+export default {
+    name: "MyPopover",
+    props: ['title'],
+    data() {
+        return {
+            showPopper: false,
+            position: {
+                top: 0,
+                left: 0
+            },
+        }
+    },
+    methods: {
+
+        onMouseOver() {
+            clearTimeout(this._timer);
+            this._timer = setTimeout(() => {
+                this.showPopper = true;
+            }, 10);
+        },
+
+        onMouseOut() {
+            clearTimeout(this._timer);
+            this._timer = setTimeout(() => {
+                this.showPopper = false;
+            }, 100);
+        },
+
+    },
+    watch: {
+        showPopper: function (val) {
+            if (val) {
+                const popover = this.$refs.popover;
+                const trigger = this.$refs.trigger.children[0];
+                // 通过placement计算出位子
+                this.position.top = trigger.offsetTop + trigger.offsetHeight;
+                popover.style.top = this.position.top + 'px';
+            }
+        }
+    },
+    mounted() {
+        this.referenceElm = this.$slots.default[0].elm;
+        this.referenceElm.onmouseover = this.onMouseOver
+        this.referenceElm.onmouseout = this.onMouseOut
+    }
+
+}
+</script>
+
+<style scoped>
+.popper {
+    width: auto;
+    background-color: #ffffff;
+    color: #212121;
+    text-align: center;
+    display: inline-block;
+    border-radius: 5px;
+    position: absolute;
+    font-size: 14px;
+    border: 1px #ebeef5 solid;
+    z-index: 200000;
+    padding: 10px 20px;
+    line-height: normal;
+    -moz-box-shadow: 6px 5px 2px -9px white, 5px 6px 2px -9px white;
+    -webkit-box-shadow: 6px 5px 2px -9px white, 5px 6px 2px -9px white;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .1);
+    margin-top: 5px;
+
+}
+</style>
--- src/views/Debug/OnlineDebug.vue
+++ src/views/Debug/OnlineDebug.vue
@@ -1,49 +1,87 @@
 <template>
-    <el-main style="padding: 10px">
-        <div style="display: flex;align-items: center;width: 100%;justify-content: center">
-            远程调试接口：
-            <div style="width: 300px">
-                <el-input v-model="interfaceAddress"></el-input>
-            </div>
-            <div style="width: 300px">
-                <el-input v-model="breakPointAddress"></el-input>
-            </div>
-            <el-tag :type="websock===null?'error':'success'">{{ websock === null ? '未连接' : '已连接' }}</el-tag>
-            <el-button style="margin-left: 10px" type="primary" @click="connectDebugBackend">连接调试后台</el-button>
-            <el-button style="margin-left: 10px" type="primary" @click="setBreakPointAddress">设置断点</el-button>
-            <el-button style="margin-left: 10px" type="primary">下一步</el-button>
-            <el-button style="margin-left: 10px" type="primary">运行到下一个断点</el-button>
-            <el-button style="margin-left: 10px" type="primary">运行到结束</el-button>
-        </div>
-        <div style="border: #B3C0D1 1px solid;margin-top: 20px;height: 500px">
-            {{ltid?'true':'false'}}
-        </div>
+    <el-container style="padding: 0">
+        <el-header style="padding: 0;line-height: 35px;height: 35px">
+            <div
+                style="display: flex;align-items: center;width: 100%;justify-content: center;border-bottom: #d1d1d1 1px solid;">
+                <div>
+                    IP地址:{{ debugVar.ip }}
+                </div>
+                <div style="margin-left: 10px">
+                    当前地址:0x{{ debugVar.currentAddress }}
+                </div>
 
-    </el-main>
+                <div class="debug-divider"/>
+                <el-tag size="mini" :type="websock===null?'error':'success'">{{
+                        websock === null ? '未连接' : '已连接'
+                    }}
+                </el-tag>
+                <MyPopover title="连接调试后台">
+                    <i class="fa fa-link debug-button  fa-fw" @click="connectDebugBackend"/>
+                </MyPopover>
+                <MyPopover title="开始运行">
+                    <i class="fa fa-play debug-button  fa-fw" @click="startRun"/>
+                </MyPopover>
+                <MyPopover title="停止运行">
+                    <i class="fa fa-stop debug-button  fa-fw" @click="stopRun"/>
+                </MyPopover>
+                <div class="debug-divider"/>
+                <div style="font-size: 14px">
+                    断点地址:0x<input style="width: 80px;" placeholder="请输入" v-model="breakPointAddress">
+                </div>
+                <MyPopover title="设置断点">
+                    <i class="fa fa-tag debug-button  fa-fw" @click="setBreakPointAddress"/>
+                </MyPopover>
+                <div class="debug-divider"/>
+
+                <MyPopover title="下一步">
+                    <i class="fa fa-mail-forward debug-button  fa-fw" @click="nextStep"/>
+                </MyPopover>
+                <MyPopover title="运行到下一个断点">
+                    <i class="fa fa-step-forward debug-button  fa-fw" @click="runToNextBreakPoint"/>
+                </MyPopover>
+                <MyPopover title="运行到结束">
+                    <i class="fa fa-stop-circle-o debug-button  fa-fw" @click="runToEnd"/>
+                </MyPopover>
+                <div class="debug-divider"/>
+                <MyPopover title="显示/隐藏日志">
+                    <i class="fa fa-terminal debug-button  fa-fw" @click="runToEnd"/>
+                </MyPopover>
+            </div>
+        </el-header>
+        <el-main>
+            <div style="border: #B3C0D1 1px solid;height: 500px">
+                {{ ltid ? 'true' : 'false' }}
+            </div>
+        </el-main>
+        <el-footer>
+            控制台
+        </el-footer>
+
+
+    </el-container>
 </template>
 
 <script>
+import {basic_url} from "@/request/request";
+import MyPopover from "@/components/Popover/MyPopover";
+
 export default {
     name: "OnlineDebug",
+    components: {MyPopover},
     props: ['id'],
     data() {
         return {
-            interfaceAddress: 'localhost:50071',
             breakPointAddress: '',
             websock: null,
+            debugVar: {},
         }
     },
     methods: {
-        //websocket
-        /**
-         * 创建websocket
-         */
         initWebSocket() {
             if (this.websock !== null) {
                 this.$message.info("已连接！")
                 return
             }
-            console.log("创建WebSocket")
             this.websock = new WebSocket("ws://127.0.0.1:8000/websocket/" + this.loginUsername)
             this.websock.onmessage = this.websocketonmessage
             this.websock.onerror = this.websocketonerror
@@ -66,6 +104,7 @@ export default {
         },
         // 数据接收
         websocketonmessage(e) {
+            //处理各种数据
             console.log('数据接收' + e.data)
         },
         // 数据发送
@@ -87,6 +126,12 @@ export default {
                 address: this.breakPointAddress
             }
             this.websock.send(JSON.stringify(data))
+        },
+        startRun() {
+
+        },
+        stopRun() {
+
         },
         nextStep() {
             let data = {
@@ -111,6 +156,14 @@ export default {
         }
     },
     mounted() {
+        this.$axios.get(basic_url + "/onlineDebug/get", {
+            params: {
+                id: this.id
+            }
+        }).then(e => {
+            this.debugVar = e.data
+            console.log(e.data)
+        })
         console.log()
     },
     computed: {
@@ -126,5 +179,52 @@ export default {
 </script>
 
 <style scoped>
+.debug-button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    color: #89898c;
+    background-color: white;
+    margin-left: 2px;
+    transition: all 300ms;
+    width: 25px;
+    height: 26px;
+}
 
+.debug-button:hover {
+
+    background-color: #dfdfdf;
+}
+
+.debug-button:active {
+    background-color: #c2bfbf;
+}
+
+.debug-divider {
+    width: 1px;
+    height: 26px;
+    background-color: #d1d1d1;
+    margin-right: 5px;
+    margin-left: 5px;
+}
+
+input {
+    font-size: 14px;
+    border: none;
+    border-bottom: black 1px solid;
+    transition: all 300ms
+}
+
+input:hover {
+    outline: 0;
+    border-bottom: #40a9ff 1px solid;
+
+}
+
+input:focus {
+    outline: 0;
+    border-bottom: #1e94ff 1px solid;
+
+}
 </style>
